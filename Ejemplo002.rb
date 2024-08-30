7d1e1e14c7ba217054d000dddf51c719544b527a
FranciscoJALG <L20290827@cdguzman.tecnm.mx>
2024-08-30 00:46:56 +0000

    Crear archivo string

---
+++ Ejemplo002.rb
@@ -0,0 +1,19 @@
+class StringManipulator
+    def reverseString(str)
+        str.reverse
+    end
+
+
+    def capitalizeString(str)
+        str.split.map(&:capitalize).join(' ')
+    end
+
+    def countVowels(str)
+        str.downcase.count('aeiou')
+    end
+end
+    manipulator = StringManipulator.new
+
+    puts manipulator.reverseString("Hola Mundo")
+    puts manipulator.capitalizeString("hola mundo")
+    puts manipulator.countVowels("Hola Mundo")
